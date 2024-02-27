c481072ae91006186b6b3af800fac527096648f8
CodeEzforme <144590228+CodeEzforme@users.noreply.github.com>
2024-02-28 00:11:40 +0700

    thay đổi vị trí sản  phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -35,6 +35,7 @@ module.exports.index = async(req, res) => {
     // hết Pagination
 
     const products = await Product.find(find)
+        .sort({ position: "desc" })
         .limit(objectPagination.limitItems)
         .skip(objectPagination.skip);
 
@@ -60,7 +61,6 @@ module.exports.changeStatus = async (req, res) => {
 
 // [PATH] /admin/products/change-multi
 module.exports.changeMulti = async (req, res) => {
-    console.log(req.body);
     const type = req.body.type;
     const ids = req.body.ids.split(", ");
 
@@ -74,6 +74,12 @@ module.exports.changeMulti = async (req, res) => {
                 deleted: true,
                 deletedAt: new Date()
             });
+            break;
+        case "change-position":
+            for (const item of ids) {
+                const [id, position] = item.split("-");
+                await Product.updateOne({ _id: id }, { position: position });
+            }
             break;
         default:
             break;
