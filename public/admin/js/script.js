8854006f6884d279c7626cb6fff660a76bee427f
CodeEzforme <144590228+CodeEzforme@users.noreply.github.com>
2024-03-05 15:25:22 +0700

    upload ảnh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -200,3 +200,19 @@ if(showAlert) {
     });
 }
 // Hết Show Alert
+
+// Upload ảnh
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+    const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+    const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        if(e.target.files.length){
+            const image = URL.createObjectURL(e.target.files[0]);
+
+            uploadImagePreview.src = image;
+        }
+    });
+}
+// Hết upload ảnh
