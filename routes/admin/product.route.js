0e7da14e27d4ed5277054e0cc4c74119a92cfba7
CodeEzforme <144590228+CodeEzforme@users.noreply.github.com>
2024-03-11 20:36:27 +0700

    Đẩy file tĩnh lên cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -2,13 +2,15 @@ const express  = require("express");
 const multer  = require("multer");
 const router = express.Router();
 
-const storageMulterHelper = require("../../helpers/storageMulter");
-const storage = storageMulterHelper();
+// const storageMulterHelper = require("../../helpers/storageMulter");
+// const s
 
-const upload = multer({ storage: storage});
+
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 
@@ -20,11 +22,23 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", upload.single("thumbnail"), validate.createPost, controller.createPost);
+router.post(
+    "/create",
+    upload.single("thumbnail"),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.createPost
+);
 
 router.get("/edit/:id", controller.edit);
 
-router.patch("/edit/:id", upload.single("thumbnail"), validate.createPost, controller.editPatch);
+router.patch(
+    "/edit/:id",
+    upload.single("thumbnail"),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.editPatch
+);
 
 router.get("/detail/:id", controller.detail);
 
