781837e03c1effef5c9838d69e86dd9c0faedb39
CodeEzforme <144590228+CodeEzforme@users.noreply.github.com>
2025-03-01 12:26:49 +0700

    fix fetch

--- public/js/script.js
+++ public/js/script.js
@@ -513,7 +513,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // kết nối sever
         // fetch('http://127.0.0.1:8000/chat/', {
-        fetch('https://5fc5-2402-800-6315-c363-3c16-e730-b8e2-9a9b.ngrok-free.app/chat/',{
+        fetch('https://7857-2402-800-6315-c363-3c16-e730-b8e2-9a9b.ngrok-free.app/chat/',{
             method: 'POST',
             // headers: {
             //     'ngrok-skip-browser-warning': 'true'  // Bỏ qua cảnh báo ngrok
